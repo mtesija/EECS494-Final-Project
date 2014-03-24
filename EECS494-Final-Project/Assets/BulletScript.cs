d8a32472d174a67b7b0ac93363025fdd64f9bff8
Michael Tesija <mtesija@umich.edu>
2014-03-24 01:15:20 -0400

    bullets + reflections

---
+++ EECS494-Final-Project/Assets/BulletScript.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BulletScript : MonoBehaviour
+{
+	private float speed = 5;
+
+	public float x = 1;
+	public float y = -1;
+
+	void Start()
+	{
+		this.rigidbody.velocity = new Vector3(x, 0, y).normalized * speed;
+	}
+
+	void FixedUpdate()
+	{
+		RaycastHit hit;
+		if(Physics.Raycast(this.transform.position, this.rigidbody.velocity, out hit, .1f))
+		{
+			this.rigidbody.velocity = Vector3.Reflect(this.rigidbody.velocity, hit.normal);
+		}
+	}
+}
---
+++ EECS494-Final-Project/Assets/spawner.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class spawner : MonoBehaviour {
+
+	public GameObject bullet;
+
+	private float timer = 0;
+
+	void Update()
+	{
+		timer -= Time.deltaTime;
+
+		if(timer <= 0)
+		{
+			GameObject bulletClone = Instantiate(bullet, this.transform.position, Quaternion.identity) as GameObject;
+			BulletScript script = bulletClone.GetComponent<BulletScript>();
+
+			float rand = Mathf.Floor(Random.Range(0, 3.999f));
+			if(rand == 0)
+			{
+				script.x = 1;
+				script.y = 1;
+			}
+			else if(rand == 1)
+			{
+				script.x = -1;
+				script.y = 1;
+			}
+			else if(rand == 2)
+			{
+				script.x = 1;
+				script.y = -1;
+			}
+			else if(rand == 3)
+			{
+				script.x = -1;
+				script.y = -1;
+			}
+
+			timer = 2;
+		}
+	}
+}
