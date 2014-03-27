632e2b5e7a8191d42385dd2c773d707f346ed7e6
Michael Tesija <mtesija@umich.edu>
2014-03-27 16:13:02 -0400

    Bullets + Initial Main Menu

---
+++ EECS494-Final-Project/Assets/Bullet/BulletScript.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BulletScript : MonoBehaviour
+{
+	private float speed = 10;
+	private Color color = Color.white;
+
+	private float hitLength = .2f;
+
+	void FixedUpdate()
+	{
+		RaycastHit hit;
+		if(Physics.Raycast(this.transform.position, this.rigidbody.velocity, out hit, hitLength))
+		{
+			if(hit.transform.tag == "Bullet")
+			{
+				return;
+			}
+			else if(hit.transform.tag == "Player")
+			{
+				//Player got hit here
+			}
+
+			this.rigidbody.velocity = Vector3.Reflect(this.rigidbody.velocity, hit.normal).normalized * speed;
+		}
+	}
+
+	public void SetColor(Color inColor)
+	{
+		color = inColor;
+		Material trailMaterial = this.GetComponent<TrailRenderer>().material;
+		trailMaterial.color = color;
+	}
+
+	public void SetSpeed(float inSpeed)
+	{
+		speed = inSpeed;
+		this.rigidbody.velocity = this.rigidbody.velocity.normalized * speed;
+	}
+}
---
+++ EECS494-Final-Project/Assets/MainMenu/SpawnerScript.cs
@@ -0,0 +1,67 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SpawnerScript : MonoBehaviour
+{
+	float redTimer = 0;
+	float greenTimer = 1;
+	float blueTimer = 2;
+	float yellowTimer = 3;
+	
+	bool redSpawn = false;
+	bool greenSpawn = false;
+	bool blueSpawn = false;
+	bool yellowSpawn = false;
+
+	float bulletSpeed = 10;
+
+	public GameObject bullet;
+
+	void Update()
+	{
+		redTimer -= Time.deltaTime;
+		greenTimer -= Time.deltaTime;
+		blueTimer -= Time.deltaTime;
+		yellowTimer -= Time.deltaTime;
+		
+		if(redTimer <= 0 && !redSpawn)
+		{
+			redSpawn = true;
+			GameObject redBullet = Instantiate(bullet, this.transform.position, Quaternion.identity) as GameObject;
+			redBullet.rigidbody.velocity = new Vector3(1, 0, 1);
+			BulletScript redBulletScript = redBullet.GetComponent<BulletScript>();
+			redBulletScript.SetColor(Color.red);
+			redBulletScript.SetSpeed(bulletSpeed);
+		}
+
+		if(greenTimer <= 0 && !greenSpawn)
+		{
+			greenSpawn = true;
+			GameObject greenBullet = Instantiate(bullet, this.transform.position, Quaternion.identity) as GameObject;
+			greenBullet.rigidbody.velocity = new Vector3(-1, 0, 1);
+			BulletScript greenBulletScript = greenBullet.GetComponent<BulletScript>();
+			greenBulletScript.SetColor(Color.green);
+			greenBulletScript.SetSpeed(bulletSpeed);
+		}
+
+		if(blueTimer <= 0 && !blueSpawn)
+		{
+			blueSpawn = true;
+			GameObject blueBullet = Instantiate(bullet, this.transform.position, Quaternion.identity) as GameObject;
+			blueBullet.rigidbody.velocity = new Vector3(1, 0, -1);
+			BulletScript blueBulletScript = blueBullet.GetComponent<BulletScript>();
+			blueBulletScript.SetColor(Color.blue);
+			blueBulletScript.SetSpeed(bulletSpeed);
+		}
+
+		if(yellowTimer <= 0 && !yellowSpawn)
+		{
+			yellowSpawn = true;
+			GameObject yellowBullet = Instantiate(bullet, this.transform.position, Quaternion.identity) as GameObject;
+			yellowBullet.rigidbody.velocity = new Vector3(-1, 0, -1);
+			BulletScript yellowBulletScript = yellowBullet.GetComponent<BulletScript>();
+			yellowBulletScript.SetColor(Color.yellow);
+			yellowBulletScript.SetSpeed(bulletSpeed);
+		}
+	}
+}
