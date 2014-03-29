72598d3bef09ddf78ab3fefbcab36782fbcbfa2e
Michael Tesija <mtesija@umich.edu>
2014-03-28 21:10:42 -0400

    Fixes for bullet hit detection

--- EECS494-Final-Project/Assets/Bullet/BulletScript.cs
+++ EECS494-Final-Project/Assets/Bullet/BulletScript.cs
@@ -6,20 +6,26 @@ public class BulletScript : MonoBehaviour
 	private float speed = 10;
 	private Color color = Color.white;
 
-	private float hitLength = .2f;
+	private float hitLength = .4f;
 
 	void FixedUpdate()
 	{
+		Vector3 position = this.transform.position;
+		if(position.x > 500 || position.x < -500 || position.y > 500  || position.y < -500 || position.z > 500 || position.z < -500)
+		{
+			Destroy(this.gameObject);
+		}
+
 		RaycastHit hit;
 		if(Physics.Raycast(this.transform.position, this.rigidbody.velocity, out hit, hitLength))
 		{
-			if(hit.transform.tag == "Bullet")
+			if(hit.transform.CompareTag("Bullet"))
 			{
 				return;
 			}
-			else if(hit.transform.tag == "Player")
+			else if(hit.transform.CompareTag("Player"))
 			{
-				//Player got hit here
+				Destroy(this.gameObject);
 			}
 
 			this.rigidbody.velocity = Vector3.Reflect(this.rigidbody.velocity, hit.normal).normalized * speed;
@@ -30,7 +36,7 @@ public class BulletScript : MonoBehaviour
 	{
 		color = inColor;
 		Material trailMaterial = this.GetComponent<TrailRenderer>().material;
-		trailMaterial.color = color;
+		trailMaterial.SetColor("_TintColor", color);
 	}
 
 	public void SetSpeed(float inSpeed)
