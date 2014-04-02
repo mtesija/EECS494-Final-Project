827dde7f94db5e0a232c5ebfe3318b93f0fa5ab6
Michael Tesija <mtesija@umich.edu>
2014-04-02 03:41:26 -0400

    player network test 2

---
+++ EECS494-Final-Project/Assets/Resources/SpawnPlayer.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SpawnPlayer : Photon.MonoBehaviour
+{
+	void Awake()
+	{
+		PhotonNetwork.Instantiate("Player", this.transform.position, this.transform.rotation, 0);
+	}
+}
