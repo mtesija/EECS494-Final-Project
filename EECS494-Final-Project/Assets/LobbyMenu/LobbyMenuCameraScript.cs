4e0a2a380a3a7efd4969332e14aff39a0983b625
Jackson <jessupjn@umich.edu>
2014-04-21 16:49:40 -0400

    Menu and lobby updates

--- EECS494-Final-Project/Assets/LobbyMenu/LobbyMenuCameraScript.cs
+++ EECS494-Final-Project/Assets/LobbyMenu/LobbyMenuCameraScript.cs
@@ -11,14 +11,15 @@ public class LobbyMenuCameraScript : MonoBehaviour
 	private bool collectBounceData = false;
 	private int playTo = 10;
 
-
+	private PhotonPlayer[] playerList;
 	private Texture2D menuColorPreview;
 
 	private Vector2 scrollPos = Vector2.zero;
 	
 	private bool connectFailed = false;
 
-	private string serverVersion = "LAMBDA";
+	public PlayerDataScript playerData;
+
 	
 	public static readonly string SceneNameMenu = "_MainMenu";
 
@@ -51,15 +52,15 @@ public class LobbyMenuCameraScript : MonoBehaviour
 
 		GameObject[] goList = new GameObject[levelNames.Length];
 		for(int i = 0; i < levelNames.Length; i++) goList[i] = new GameObject(levelNames[i]);
-		levelBoxControl = new ObjectComboBox<GameObject>(new Rect(65, 50, 200, 20), goList, listStyle);
+		levelBoxControl = new ObjectComboBox<GameObject>(new Rect(65, 70, 200, 20), goList, listStyle);
 
 		goList = new GameObject[colorNames.Length];
 		for(int i = 0; i < colorNames.Length; i++) goList[i] = new GameObject(colorNames[i]);
-		colorBoxControl = new ObjectComboBox<GameObject>(new Rect(70, 450, 150, 20), goList, listStyle);
+		colorBoxControl = new ObjectComboBox<GameObject>(new Rect(315, 100, 100, 20), goList, listStyle);
 
 		goList = new GameObject[modeNames.Length];
 		for(int i = 0; i < modeNames.Length; i++) goList[i] = new GameObject(modeNames[i]);
-		modeBoxControl = new ObjectComboBox<GameObject>(new Rect(65, 315, 200, 20), goList, listStyle);
+		modeBoxControl = new ObjectComboBox<GameObject>(new Rect(65, 335, 200, 20), goList, listStyle);
 	}
 	
 
@@ -69,68 +70,70 @@ public class LobbyMenuCameraScript : MonoBehaviour
 		PhotonNetwork.automaticallySyncScene = true;
 		
 		// generate a name for this player, if none is assigned yet
-		if (String.IsNullOrEmpty(PhotonNetwork.playerName))
-		{
-			PhotonNetwork.playerName = "Guest" + Random.Range(1, 9999);
-		}
 		roomName = "Game" + Random.Range (1, 9999);
 		menuColorPreview = new Texture2D(150, 90);
+
+	}
+
+	public void Update()
+	{
 
 	}
 	
 	public void OnGUI()
 	{
-//		if (!PhotonNetwork.connected)
-//		{
-//			if (PhotonNetwork.connecting)
-//			{
-//				GUILayout.Label("Connecting to: " + PhotonNetwork.ServerAddress);
-//			}
-//			else
-//			{
-//				GUILayout.Label("Not connected. Check console output. Detailed connection state: " + PhotonNetwork.connectionStateDetailed + " Server: " + PhotonNetwork.ServerAddress);
-//			}
-//			
-//			if (this.connectFailed)
-//			{
-//				GUILayout.Label("Connection failed. Check setup and use Setup Wizard to fix configuration.");
-//				GUILayout.Label(String.Format("Server: {0}", new object[] {PhotonNetwork.ServerAddress}));
-//				GUILayout.Label("AppId: " + PhotonNetwork.PhotonServerSettings.AppID);
-//				
-//				if (GUILayout.Button("Try Again", GUILayout.Width(100)))
-//				{
-//					this.connectFailed = false;
-//					PhotonNetwork.ConnectUsingSettings(serverVersion);
-//				}
-//			}
-//			
-//			return;
-//		}
-		
-		
+		GUIStyle st = new GUIStyle();
+		st.fontStyle = FontStyle.Bold;
+		st.alignment = TextAnchor.MiddleCenter;
+		st.normal.textColor = Color.white;
+
+		GUIStyle pst = new GUIStyle(st);
+		pst.normal.textColor = Color.green;
+
 		GUI.skin.box.fontStyle = FontStyle.Bold;
-		GUI.Box(new Rect((Screen.width - 800) / 2, (Screen.height - 600) / 2, 800, 600), "Lobby: ");
+		GUI.skin.box.fontSize = 35;
+		GUI.Box(new Rect((Screen.width - 800) / 2, (Screen.height - 600) / 2, 800, 600), "Lobby: "+ PhotonNetwork.room.name);
 		GUILayout.BeginArea(new Rect((Screen.width - 800) / 2, (Screen.height - 600) / 2, 500, 600));
 		
-		GUILayout.Space(50);
+		GUILayout.Space(70);
 		GUILayout.BeginHorizontal();
-		GUILayout.Label("Player Name:", GUILayout.Width(100));
+		GUILayout.Space(15);
+		GUILayout.Label("Player Name:", st, GUILayout.Width(100));
 		GUILayout.Space(200);
-		GUILayout.Label("Player Color:", GUILayout.Width(100));
+		GUILayout.Label("Player Color:", st, GUILayout.Width(100));
 		GUILayout.EndHorizontal();
 		GUILayout.Space(20);
 
+		GUILayout.BeginHorizontal();
+		GUILayout.Space(15);
+		GUILayout.Label(PhotonNetwork.player.name, pst, GUILayout.Width(100));
+		GUILayout.Space(200);
+		colorBoxControl.Show();
+		GUILayout.Space(15);
+		GUILayout.EndHorizontal();
 
+		GUILayout.Space(10);
+		GUILayout.BeginHorizontal();
+		GUILayout.Space(15);
+		GUILayout.Label("PLAYER", st, GUILayout.Width(100));
+		GUILayout.Space(200);
+		GUILayout.Label("COLOR", st, GUILayout.Width(100));
+		GUILayout.Space(15);
+		GUILayout.EndHorizontal();
+		
 		// PLAYERS AND THEIR COLORS
-		int numPlayers = 6;
-		for(int i = 0; i < numPlayers; i++)
+		foreach(PhotonPlayer p in PhotonNetwork.playerList)
 		{
+			GUILayout.Space(10);
+
+			if(p.name == PhotonNetwork.player.name) break;
+
 			GUILayout.BeginHorizontal();
 			GUILayout.Space(15);
 
-			GUILayout.Label("Player " + i.ToString(), GUILayout.Width(100));
+			GUILayout.Label(p.name, st, GUILayout.Width(100));
 			GUILayout.Space(200);
-			GUILayout.Label("Color:", GUILayout.Width(100));
+			GUILayout.Label("COLOR", st, GUILayout.Width(100));
 
 			GUILayout.Space(15);
 			GUILayout.EndHorizontal();
@@ -141,7 +144,7 @@ public class LobbyMenuCameraScript : MonoBehaviour
 
 		// SECOND COLUMN WITH OPTIONS
 		GUILayout.BeginArea(new Rect(((Screen.width - 800) / 2) + 500, (Screen.height - 600) / 2, 300, 600));
-		GUILayout.Space(50);
+		GUILayout.Space(70);
 		GUILayout.BeginHorizontal();
 		GUILayout.Label("Map:", GUILayout.Width(60));
 		GUILayout.EndHorizontal();
@@ -199,147 +202,12 @@ public class LobbyMenuCameraScript : MonoBehaviour
 		GUILayout.EndHorizontal();
 		GUILayout.EndArea();
 
-
-
-
-
-
-
-
-
-
-
-
-
-//		// Player name
-//		GUILayout.BeginHorizontal();
-//		GUILayout.Space(15);
-//		GUILayout.Label("Player Name:", GUILayout.Width(80));
-//		PhotonNetwork.playerName = GUILayout.TextField(PhotonNetwork.playerName);
-//		if (GUI.changed)
-//		{
-//			// Save name
-//			PlayerPrefs.SetString("playerName", PhotonNetwork.playerName);
-//		}
-//		GUILayout.Space(15);
-//		GUILayout.EndHorizontal();
-//		
-//		GUILayout.BeginHorizontal();
-//		GUILayout.Space(15);
-//		GUILayout.BeginVertical();
-//		GUILayout.Label("Player Color:", GUILayout.Width(100));
-//		GUILayout.BeginHorizontal();
-//		GUILayout.Label("R:", GUILayout.Width(15));
-//		r = GUILayout.HorizontalSlider(r, .2f, 1, GUILayout.Width(180));
-//		GUILayout.EndHorizontal();
-//		GUILayout.BeginHorizontal();
-//		GUILayout.Label("G:", GUILayout.Width(15));
-//		g = GUILayout.HorizontalSlider(g, .2f, 1, GUILayout.Width(180));
-//		GUILayout.EndHorizontal();
-//		GUILayout.BeginHorizontal();
-//		GUILayout.Label("B:", GUILayout.Width(15));
-//		b = GUILayout.HorizontalSlider(b, .2f, 1, GUILayout.Width(180));
-//		GUILayout.EndHorizontal();
-//		GUILayout.EndVertical();
-//		GUILayout.Space(10);
-//		playerColor = new Color(r, g, b);
-//		//playerData.playerColor = playerColor;
-//		GUI.color = playerColor;
-//		GUILayout.Label(menuColorPreview);
-//		GUI.color = Color.white;
-//		GUILayout.Space(15);
-//		GUILayout.EndHorizontal();
-//		
-//		GUILayout.BeginHorizontal();
-//		GUILayout.Space(10);
-//		collectDeathData = GUILayout.Toggle(collectDeathData, "Collect Death Data");
-//		//playerData.collectDeathData = collectDeathData;
-//		collectHitData = GUILayout.Toggle(collectHitData, "Collect Hit Data");
-//		//playerData.collectHitData = collectHitData;
-//		collectBounceData = GUILayout.Toggle(collectBounceData, "Collect Bounce Data");
-//		//playerData.collectBounceData = collectBounceData;
-//		GUILayout.Space(10);
-//		GUILayout.EndHorizontal();
-//
-//		GUILayout.Space(10);
-//
-//		// Join room by title
-//		GUILayout.BeginHorizontal();
-//		GUILayout.Space(15);
-//		GUILayout.Label("Room Name:", GUILayout.Width(80));
-//		this.roomName = GUILayout.TextField(this.roomName);
-//		GUILayout.Space(15);
-//		GUILayout.EndHorizontal();
-//
-//		
-//		GUILayout.BeginHorizontal();
-//		GUILayout.Space(15);
-//		GUILayout.Label("Select Map:", GUILayout.Width(80));
-//		levelSelect = GUILayout.SelectionGrid(levelSelect, levelNames, 3);
-//		GUILayout.Space(10);
-//		if (GUILayout.Button("Create Room", GUILayout.Width(100)))
-//		{
-//			PhotonNetwork.CreateRoom(this.roomName, new RoomOptions() { maxPlayers = 5 }, null);
-//		}
-//		GUILayout.Space(15);
-//		GUILayout.EndHorizontal();
-//
-//		GUILayout.Space(10);
-//
-//		GUILayout.BeginHorizontal();
-//		GUILayout.Space(15);
-//		if(PhotonNetwork.countOfPlayers == 1)
-//		{
-//			GUILayout.Label("There is " + PhotonNetwork.countOfPlayers + " user online playing in " + PhotonNetwork.countOfRooms + " games.");
-//		}
-//		else
-//		{
-//			GUILayout.Label("There are " + PhotonNetwork.countOfPlayers + " users online playing in " + PhotonNetwork.countOfRooms + " games.");
-//		}
-//		GUILayout.Space(15);
-//		GUILayout.EndHorizontal();
-//
-//		GUILayout.Space(10);
-//
-//		if (PhotonNetwork.GetRoomList().Length == 0)
-//		{
-//			GUILayout.BeginHorizontal();
-//			GUILayout.Space(15);
-//			GUILayout.Label("No games are currently available.");
-//			GUILayout.EndHorizontal();
-//		}
-//		else
-//		{
-//			GUILayout.BeginHorizontal();
-//			GUILayout.Space(15);
-//			GUILayout.BeginVertical();
-//			GUILayout.Label(PhotonNetwork.GetRoomList().Length + " games are currently available:");
-//			
-//			// Room listing: simply call GetRoomList: no need to fetch/poll whatever!
-//			this.scrollPos = GUILayout.BeginScrollView(this.scrollPos);
-//			foreach (RoomInfo roomInfo in PhotonNetwork.GetRoomList())
-//			{
-//				GUILayout.BeginHorizontal();
-//				GUILayout.Label(roomInfo.name + " " + roomInfo.playerCount + "/" + roomInfo.maxPlayers);
-//				if (GUILayout.Button("Join"))
-//				{
-//					PhotonNetwork.JoinRoom(roomInfo.name);
-//				}
-//				GUILayout.EndHorizontal();
-//			}
-//			
-//			GUILayout.EndScrollView();
-//
-//			GUILayout.EndHorizontal();
-//			GUILayout.Space(15);
-//			GUILayout.EndVertical();
-//		}
-//
-//		GUILayout.EndArea();
 	}
 	
 	public void StartGame()
 	{
+		if(PhotonNetwork.playerList.Length <= 1) return;
+
 		switch(levelBoxControl.SelectedItemIndex)
 		{
 		case 0:
